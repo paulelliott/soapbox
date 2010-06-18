15857817eb5806b37a81cfa20df6fa5c2e94a466
Paul Elliott <paul@hashrocket.com>
2010-06-18 17:18:53 -0400

    Add spec for post#to_param

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe Post do
+
+  describe "#to_param" do
+
+    let(:post) { Fabricate(:post, :title => "something awesome") }
+
+    it 'returns the permalink' do
+      post.to_param.should == 'something-awesome'
+    end
+
+  end
+
 end
